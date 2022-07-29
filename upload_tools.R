7d4062b5b74ade6721b9672127d111321105adb4
He <her2@nciws-d2335-v.nci.nih.gov>
2022-07-29 10:31:40 -0400

    further developed and refactored  upload tools

--- upload_tools.R
+++ upload_tools.R
@@ -1,230 +1,237 @@
+# Dev Note
+# The upload_tools.R was developed by Robin, 
+# Further development and refactoring was by Rui
+# Current directory is 
+# /rstudio-files/ccbr-data/git-repos/nidap-export
+# This is the refactored version, original file can be found in 
+# /rstudio-files/ccbr-data/RSK/nidap_transfer_scripts2/
+
+
+# Library
 library(RCurl)
 library(httr)
+library(jsonlite)
 
-create_folder<-function(path,pathname,key){
-
-  callurl<-paste0("https://nidap.nih.gov/compass/api/paths/",path,"/",pathname)
-
-  rma<-PUT(url=callurl,add_headers(Authorization = paste("Bearer", key, sep = " ")))
-  con<-content(rma,"parsed")
-  cont<-content(rma,"text")
+# Create_folder is the function to generate a folder on nidap
+create_folder <- function(path, pathname, key){
+  
+  callurl <- paste0("https://nidap.nih.gov/compass/api/paths/", path, "/", pathname)
+  
+  rma <- PUT(url = callurl, 
+           add_headers(Authorization = paste("Bearer", key, sep = " ")))
+  con <- content(rma, "parsed")
+  cont <- content(rma, "text")
   cont
   return(con$rid)
 }
 
+# Upload_project is the function to upload the dataset from local onto NIDAP
+upload_project <- function(upload_file_name, datasetname, folder_path_on_NIDAP, key){
 
-#rid="ri.foundry.main.dataset.0950a6c3-fcae-45cd-b3f0-2386adb919a5"
-upload_project<-function(upload_file_name,datasetname,email,key){
+  # Services address on NIDAP
+  url = "https://nidap.nih.gov/"
+  dataproxy = "foundry-data-proxy/api/dataproxy/datasets/"
+  catalog = "foundry-catalog/api/catalog/datasets/"
 
+  # Get address to the catalog service
+  callurlcreate <- paste0(url , "foundry-catalog/api/catalog/datasets")
+  rmc <- POST(url = callurlcreate, 
+                add_headers(Authorization = paste("Bearer", key, sep = " ")), 
+                verify = FALSE, 
+                content_type_json(), 
+                body = list(path = paste0(folder_path_on_NIDAP, "/", datasetname)), 
+                encode = "json")
+  rmcc <- content(rmc, "parsed")
 
-  #pushnidap<-function(key,rid){
-  #curl -X POST -H "Authorization: Bearer $FOUNDRYTOKEN" https://nidap.nih.gov/foundry-data-proxy/api/dataproxy/datasets/$DATASET_RID/transactions/$TRANSACTION_RID/putFile?logicalPath=$path --data-binary "@{}";' \;
+  # Create Branch
+  callurlcreatebranch <- paste0(url , 
+                            "foundry-catalog/api/catalog/datasets/", 
+                            rmcc$rid, 
+                            "/branchesUnrestricted2/master")
+  rmcb <- POST(url = callurlcreatebranch, 
+              add_headers(Authorization = paste("Bearer", key, sep = " ")), 
+              verify = FALSE, 
+              content_type_json(), 
+              body = '{}', 
+              encode = "raw")
+  rmccb <- content(rmcb, "parsed")
 
-  url="https://nidap.nih.gov/"
-  dataproxy="foundry-data-proxy/api/dataproxy/datasets/"
-  catalog="foundry-catalog/api/catalog/datasets/"
+  # Create Transaction
+  callurl <- paste0(url , catalog , rmcc$rid , '/transactions')
+  rm <- POST(url = callurl, 
+            add_headers(Authorization = paste("Bearer", key, sep = " ")), 
+            verify = FALSE, 
+            content_type_json(), 
+            body = list(branchId = "master"), 
+            encode = "json")
+  con <- content(rm, "parsed")
+  cont <- content(rm, "text")
 
-
-  #Create Folder PATH
-
-
-
-
-  ################3
-  #Create Dataset
-  callurlcreate<-paste0(url ,"foundry-catalog/api/catalog/datasets")
-  # print(callurl)
-  rmc<-POST(url=callurlcreate,add_headers(Authorization = paste("Bearer", key, sep = " ")), verify=FALSE,
-            content_type_json(),body = list(path=paste0(email,"/",datasetname)),encode = "json")
-  rmcc<-content(rmc,"parsed")
-  #jsonlite::prettify(content(rmc,"text"))
-
-
-  ##############
-  #Create Branch
-  callurlcreatebranch<-paste0(url ,"foundry-catalog/api/catalog/datasets/",rmcc$rid,"/branchesUnrestricted2/master")
-  # print(callurl)
-  rmcb<-POST(url=callurlcreatebranch,add_headers(Authorization = paste("Bearer", key, sep = " ")), verify=FALSE,
-             content_type_json(),body = '{}', encode = "raw")
-  rmccb<-content(rmcb,"parsed")
-  #jsonlite::prettify(content(rmcb,"text"))
-
-
-
-
-
-  #############
-  #Create Transaction
-
-  callurl<-paste0(url ,catalog , rmcc$rid , '/transactions')
-  # print(callurl)
-  rm<-POST(url=callurl,add_headers(Authorization = paste("Bearer", key, sep = " ")), verify=FALSE,
-           content_type_json(),body = list(branchId="master"),encode = "json")
-
-
-  con<-content(rm,"parsed")
-  cont<-content(rm,"text")
-  #jsonlite::prettify(content(rm,"text"))
-
-  #############
   #Upload File
-
-  trid<-con$rid
-  print( trid)
-  brid<-con$datasetRid
-
-
-  file_url = paste0(url,dataproxy,brid,"/transactions/",trid,"/putFile?logicalPath=",upload_file_name)
-
-
-  resp2=POST(url = file_url,add_headers(Authorization = paste("Bearer", key, sep = " ")),body = upload_file(upload_file_name))
+  trid <- con$rid
+  print(trid)
+  brid <- con$datasetRid
+  file_url = paste0(url, dataproxy, brid, 
+                    "/transactions/", trid, 
+                    "/putFile?logicalPath = ", 
+                    upload_file_name)
+  resp2 = POST(url = file_url, 
+               add_headers(Authorization = paste("Bearer", key, sep = " ")), 
+               body = upload_file(upload_file_name))
 
   print(resp2)
-  #jsonlite::prettify(content(resp2,"text"))
-  #############
-  #commit transaction
 
-  callurlabort<-paste0(url ,catalog , brid , '/transactions/',trid,'/commit')
-  # print(callurl)
-  rma<-POST(url=callurlabort,add_headers(Authorization = paste("Bearer", key, sep = " ")), verify=FALSE,
-            content_type_json(),body = '{}',encode = "raw")
-  #"record": {}
-  #body = '{}', encode = "raw")
-
+ # Commit transaction
+  callurlabort <- paste0(url , catalog , brid , '/transactions/', trid, '/commit')
+  rma <- POST(url = callurlabort, 
+              add_headers(Authorization = paste("Bearer", key, sep = " ")), 
+              verify = FALSE, 
+              content_type_json(), 
+              body = '{}', 
+              encode = "raw")
   rma
-  #jsonlite::prettify(content(rma,"text"))
-  return(brid)
+ return(brid)
 }
 
-
-getDestinations<-function(key){
-  #############
-  #Create Transaction
-  callurl<-"https://nidap.nih.gov/phonograph2/api/search/tables"
-  requestbody<-'{
-        "tableRids": [
-            "ri.phonograph2.main.table.f2b7fa90-558e-4cbf-9636-8c0d28da820a"
-        ],
-        "filter": {
-            "type": "matchAll",
-            "matchAll": {}
-        },
-        "aggregations": {}
-    }'
-  # print(callurl)
-  rm<-POST(url=callurl,add_headers(Authorization = paste("Bearer", key, sep = " ")), verify=FALSE,
-           content_type_json(),body = requestbody,encode = "json")
-
-
-  con<-content(rm,"parsed")
-  cont<-content(rm,"text")
-
-  return(fromJSON(cont)$hits[, 3][,2])
+# getDestinations is the function to get the location of the phonograph2 dataset from NIDAP
+getDestinations <- function(key, phonograph2tableRID){
+ # Create Transaction
+  callurl <- "https://nidap.nih.gov/phonograph2/api/search/tables"
+  requestbody <- paste0('{
+  "tableRids": [', 
+  tableRID, 
+  '], 
+  "filter": {
+   "type": "matchAll", 
+   "matchAll": {}
+  }, 
+  "aggregations": {}
+ }')
+  rm <- POST(url = callurl, add_headers(Authorization = paste("Bearer", key, sep = " ")), verify = FALSE, 
+  content_type_json(), body = requestbody, encode = "json")
+  con <- content(rm, "parsed")
+  cont <- content(rm, "text")
+  return(fromJSON(cont)$hits[, 3][, 2])
 }
 
-getRidFromPath<-function(key,path){
-  callurl<-"https://nidap.nih.gov/compass/api/resources?path=/Users/robin.kramer@nih.gov/Atest/al6"
-
-  rma<-GET(url=callurl,add_headers(Authorization = paste("Bearer", key, sep = " ")))
-  con<-content(rma,"parsed")
-  cont<-content(rma,"text")
+# getRidFromPath is used to get RID of a existing NIDAP file
+getRidFromPath <- function(key, file_path_on_NIDAP){
+  callurl <- paste0("https://nidap.nih.gov/compass/api/resources?path=", file_path_on_NIDAP)
+  rma <- GET(url = callurl, 
+          add_headers(Authorization = paste("Bearer", key, sep = " ")))
+  con <- content(rma, "parsed")
+  cont <- content(rma, "text")
   cont
   return(con$rid)
 }
 
-#counts_rid="ri.foundry.main.dataset.13674385-c194-4fb0-9496-cb123b669d3e"
-#metadata_rid="ri.foundry.main.dataset.c9554360-8c56-45e7-98bf-f22de80067c4"
-#todo: get rid from path
-#path_rid="ri.compass.main.folder.852043ba-4f9d-41b2-9103-ee00bc454364"
-#wname="A4"
-#createWorkBook(key,counts_rid,metadata_rid,path_rid,wname)
-createWorkBook<-function(key,counts_rid,metadata_rid,frid,wname){
+# getNameFromRid is used to get name of the rid
+getNameFromRid <- function(key, RID){
+  callurl <- paste0("https://nidap.nih.gov/compass/api/resources/", 
+                RID, 
+                "/path-json")
+  rma <- GET(url = callurl, 
+             add_headers(Authorization = paste("Bearer", key, sep = " ")))
+  con <- content(rma, "parsed")
+  result <- sub('.*\\/', '', con)
+  cont <- content(rma, "text")
+  cont
+  return(result)
+}
 
-  #create workbook
-  #curl -XPOST -k  -H "Accept: application/json" -H "Content-Type: application/json"  -H "Authorization: Bearer ${token}" "https://nidap.nih.gov/vector/api/workbooks/" -o created.json -d @create_workbook.json
+# getPathFromRid is used to get path of the rid
+getPathFromRid <- function(key, RID){
+  callurl <- paste0("https://nidap.nih.gov/compass/api/resources/", 
+                    RID, 
+                    "/path-json")
+  rma <- GET(url = callurl, 
+             add_headers(Authorization = paste("Bearer", key, sep = " ")))
+  con <- content(rma, "parsed")
+  result <- sub('.*\\/', '', con)
+  cont <- content(rma, "text")
+  cont
+  return(con)
+}
 
-  path_rid<-frid   #getRidFromPath(path,key)
-
-
-  callurl="https://nidap.nih.gov/vector/api/workbooks/"
-  requestbody=paste0('{
-    "parentFolderRid": "',path_rid,'",
-    "name": "',wname,'"
-  }')
-  rma<-POST(url=callurl,add_headers(Authorization = paste("Bearer", key, sep = " ")), verify=FALSE,
-            content_type_json(),body = requestbody,encode = "json")
-
-
-  con<-content(rma,"parsed")
-  cont<-content(rma,"text")
-
-  callurl=paste0( "https://nidap.nih.gov/vector/api/workbooks/",con$workbook$rid,"/branches/master/logicNodes-batchCreate")
-  requestbody=paste0('{
-        "requests": [
-            {
-                "displayOptions": {
-                    "dimensions": {
-                        "height": 200,
-                        "width": 300
-                    },
-                    "position": {
-                        "left": -200,
-                        "top": 300
-                    },
-                    "viewMode": "DATA_PREVIEW"
-                },
-                "inputs": [],
-                "outputAlias": "counts_matrix",
-                "spec": {
-                    "importedDataset": {
-                        "datasetRid": "',counts_rid,'"
-                    },
-                    "type": "importedDataset"
-                }
-            }
-        ]
-    }')
-
-  rma2<-POST(url=callurl,add_headers(Authorization = paste("Bearer", key, sep = " ")), verify=FALSE,
-             content_type_json(),body = requestbody,encode = "json")
-
-
-  con2<-content(rma2,"parsed")
-  cont2<-content(rma2,"text")
-  requestbody=paste0('{
-        "requests": [
-  {
-    "displayOptions": {
-      "dimensions": {
-        "height": 200,
+# createWorkBook adds dataset as nodes in an existing NIDAP workbook
+createWorkBook <- function(key, 
+                           list_of_dataset_rids, 
+                           folder_path, 
+                           workbook_name,
+                           branch){
+  
+  # Get rid of the workbook
+  path_rid <- getRidFromPath(key, folder_path)
+  
+  #Get workbook rid
+  workbook_path <- paste0(folder_path, "/", workbook_name)
+  print(workbook_path)
+  workbook_rid <- getRidFromPath(key, workbook_path)
+  print(workbook_rid)
+  
+  # Locate the add node service
+  callurl = paste0( "https://nidap.nih.gov/vector/api/workbooks/", 
+                    workbook_rid, 
+                    "/branches/",branch,"/logicNodes-batchCreate")
+  
+  # Position of datasets
+  start_x_posision <- 200
+  x_position_step <- 20
+  start_y_posision <- 300
+  y_position_step <- 60
+  step_count <- 0
+  # Loop through the list of datasets to post nodes into targeted workbook
+  for(dataset_rids in list_of_dataset_rids){
+    current_x_position <- start_x_posision + x_position_step * step_count
+    current_y_position <- start_y_posision + y_position_step * step_count
+    step_count <- step_count + 1
+    dataset_alias <- getNameFromRid(key, dataset_rids)
+    dataset_alias <- sub(" ", "_", dataset_alias)
+    requestbody = paste0('{
+    "requests": [
+     {
+      "displayOptions": {
+       "dimensions": {
+        "height": 200, 
         "width": 300
-      },
-      "position": {
-        "left": 200,
-        "top": 300
-      },
-      "viewMode": "DATA_PREVIEW"
-    },
-    "inputs": [],
-    "outputAlias": "sample_metadata",
-    "spec": {
-      "importedDataset": {
-        "datasetRid": "',metadata_rid,'"
-      },
-      "type": "importedDataset"
-    }
+       }, 
+       "position": {
+        "left": ', current_x_position, ', 
+        "top": ', current_y_position, '
+       }, 
+       "viewMode": "DATA_PREVIEW"
+      }, 
+      "inputs": [], 
+      "outputAlias": "', dataset_alias,'",
+      "spec": {
+       "importedDataset": {
+        "datasetRid": "', dataset_rids, '"
+       }, 
+       "type": "importedDataset"
+      }
+     }
+    ]
+   }')
+
+    response_from_NIDAP <- POST(url = callurl, 
+              add_headers(Authorization = paste("Bearer", key, sep = " ")), 
+              verify = FALSE, 
+              content_type_json(), 
+              body = requestbody, 
+              encode = "json")
+    response_content_table <- content(response_from_NIDAP, "parsed")
+    response_content_text <- content(response_from_NIDAP, "text")
+    
+    # Report status of action
+    if(is.null(response_content_table$errorCode)){
+      print(paste0("Success imported: ", dataset_alias))
+      }else{
+      print(paste0("Fail to imported: ", dataset_alias, 
+                   "\n", response_content_table$errorCode))
+      }
   }
-        ]
-    }')
-
-  rma3<-POST(url=callurl,add_headers(Authorization = paste("Bearer", key, sep = " ")), verify=FALSE,
-             content_type_json(),body = requestbody,encode = "json")
-
-
-  con3<-content(rma3,"parsed")
-  cont3<-content(rma3,"text")
-
-  return(con$workbook$rid)
+  
+ return(response_content_table$workbook$rid)
 }
 
 
