672bbb02f10a871594491ba4e9836bddebb16832
Rui He <her2@nih.gov>
2023-03-22 11:58:53 -0400

    Update github generation script

--- prepare_github_folder.sh
+++ prepare_github_folder.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
+set -e
+
 echo "WARNING: THIS METHOD IS INTENDED TO BE USED IN A BLANK GITHUB REPO"
 echo "WARNING: IT MAY OVERWRITE ORIGINAL CONTENTS IN THE REPO "
 
-
+echo "Log for updating GitHub repo\n" > github_repo_udpate_log.log
+exec >> github_repo_udpate_log.log
 # Parse command-line arguments
-while getopts "github:rds:pipeline:docker:" opt; do
-  case $opt in
-    github) github_repo="$OPTARG";;
-    rds) rds="$OPTARG";;
-    pipeline) pipeline="$OPTARG";;
-    docker) docker="$OPTARG";;
+while getopts "g:r:p:d:" opt; do
+  case "${opt}" in
+    g) github_repo="$OPTARG";;
+    r) rds="$OPTARG";;
+    p) pipeline="$OPTARG";;
+    d) docker="$OPTARG";;
     \?) echo "Invalid option -$OPTARG" >&2; exit 1;;
     :) echo "Option -$OPTARG requires an argument" >&2; exit 1;;
   esac
 done
 
+echo "github_repo=$github_repo"
+echo "rds=$rds"
+echo "pipeline=$pipeline"
+echo "docker=$docker\n"
+
 # Check if required input argument is present
-if [ -z "$github_repo" ] || [ -z "$rds" ] || [ -z "$pipeline" ]|| [ -z "$docker" ]; then
+if [ -z "${github_repo}" ] || [ -z "${rds}" ] || [ -z "${pipeline}" ] || [ -z "${docker}" ]; then
   echo "Missing required input argument, should have github, rds, pipeline, docker" >&2
   exit 1
 fi
@@ -28,30 +36,32 @@ day=$(date '+%d')
 month=$(date '+%m')
 year=$(date '+%Y')
 
-echo "Preparing source git repo from GitHub: "
+echo "\nPreparing source git repo from GitHub: \n"
 echo "$github_repo"
 
 file_dir="$(dirname "$(readlink -f "$0")")"
 
-echo "Changing workind directory to one level up."
+echo "\nChanging workind directory to one level up.\n"
 cd $file_dir/../
 
-echo "Cloning target GitHub repo"
+echo "\nCloning target GitHub repo\n"
 
 logfile="github_creation_log_$year$month$day$hour.log"
 
 echo $(git clone $github_repo) > $logfile
 
 github_foler="$(grep -o "'[^']*'" $logfile)"
-
+github_foler=$(echo $github_foler | tr -d "'")
 rm $logfile
+
+echo "\nGitHub Folder is: $github_foler\n"
 
 cd ./$github_foler
 
-echo "Copying the template folder to the GitHub repo"
-cp $file_dir/github_template/* .
+echo "\nCopying the template folder to the GitHub repo\n"
+cp -R $file_dir/github_template/* .
 
-echo "Copying the template-R function and run-pipeline.sh into src folder:"
+echo "\nCopying the template-R function and run-pipeline.sh into src folder:\n"
 cp $file_dir/../$pipeline/template_*.R ./src
 
 if [ "$rds" = "Yes" ]; then
@@ -60,16 +70,16 @@ if [ "$rds" = "Yes" ]; then
   echo "WARNING: PLEASE MAKE SURE YOU WANT TO SHARE THE DATA ON GITHUB"
   cp -R $file_dir/../$pipeline/rds_output ./src
 else
-  echo "RDS is not set to Yes, not including the rds outputs in the github repo"
+  echo "\nRDS is not set to Yes, not including the rds outputs in the github repo\n"
   mkdir ./src/rds_output
-  echo "This file is for maintaining folder structure." > ./src/rds_output/.githold 
+  echo "\nThis file is for maintaining folder structure.\n" > ./src/rds_output/.githold 
 fi
 
-echo "Copying the Dockerfile to the GitHub repo:"
+echo "\nCopying the Dockerfile to the GitHub repo:\n"
 cp -R $file_dir/../$docker/* ./Docker_file
 
 
-echo "Committing the changes"
+echo "\nCommitting the changes\n"
 git add --all
 git commit -a -m "Update the GitHub Folder"
 
